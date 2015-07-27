963792667454b96759d45f95fe514afbaf17f39b
Coleman Cox <colezane82@gmail.com>
2015-07-27 08:38:31 -0500

    updating hw

--- Brocfile.js
+++ Brocfile.js
@@ -1,10 +1,11 @@
 var merge = require("broccoli-merge-trees");
 var sassCompile = require("broccoli-sass");
+var compiledCss = sassCompile(includePaths,'app.scss', 'app.css');
+
 var includePaths = [
 "assets",
 "bower_components/reset-css",
 "bower_components/fontawesome/scss",
 "bower_components/bourbon/app/assets/stylesheets",
 "bower_components/neat/app/assets/stylesheets"];
-var compiledCss = sassCompile(includePaths,'app.scss', 'app.css');
  module.exports = merge(['public', compileCss]);
